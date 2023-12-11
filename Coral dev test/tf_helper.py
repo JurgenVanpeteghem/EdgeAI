bb8ddebc8a7544a510a2faa4be029181c7c16ecc
JurgenVanpeteghem <116504027+JurgenVanpeteghem@users.noreply.github.com>
2023-12-11 19:53:47 +0100

    Update tf_helper.py

--- Coral dev test/tf_helper.py
+++ Coral dev test/tf_helper.py
@@ -1,50 +1,45 @@
 import numpy as np
-import tensorflow as tf
-
 
 # Set the seed value for experiment reproducibility.
 seed = 42
-tf.random.set_seed(seed)
 np.random.seed(seed)
+
+def stft(waveform, frame_length=255, frame_step=128):
+    # Compute Short-Time Fourier Transform (STFT)
+    window = np.hanning(frame_length)
+    frames = np.lib.stride_tricks.sliding_window_view(waveform, window_shape=(frame_length,), step=frame_step)
+    stft_result = np.fft.fft(frames * window, axis=-1)
+    return np.abs(stft_result)
 
 def get_spectrogram(waveform):
     # Zero-padding for an audio waveform with less than 16,000 samples.
     input_len = 16000
     waveform = waveform[:input_len]
-    zero_padding = tf.zeros(
-        [16000] - tf.shape(waveform),
-        dtype=tf.float32)
-    # Cast the waveform tensors' dtype to float32.
-    waveform = tf.cast(waveform, dtype=tf.float32)
-    # Concatenate the waveform with `zero_padding`, which ensures all audio
-    # clips are of the same length.
-    equal_length = tf.concat([waveform, zero_padding], 0)
-    # Convert the waveform to a spectrogram via a STFT.
-    spectrogram = tf.signal.stft(
-        equal_length, frame_length=255, frame_step=128)
-    # Obtain the magnitude of the STFT.
-    spectrogram = tf.abs(spectrogram)
-    # Add a `channels` dimension, so that the spectrogram can be used
-    # as image-like input data with convolution layers (which expect
-    # shape (`batch_size`, `height`, `width`, `channels`).
-    spectrogram = spectrogram[..., tf.newaxis]
-    return spectrogram
+    zero_padding = np.zeros(16000 - len(waveform), dtype=np.float32)
+    # Concatenate the waveform with zero_padding, ensuring all audio clips are of the same length.
+    equal_length = np.concatenate([waveform, zero_padding])
 
+    # Convert the waveform to a spectrogram via STFT.
+    spectrogram = stft(equal_length)
+
+    # Add a `channels` dimension.
+    spectrogram = spectrogram[..., np.newaxis]
+    
+    return spectrogram
 
 def preprocess_audiobuffer(waveform):
     """
     waveform: ndarray of size (16000, )
     
-    output: Spectogram Tensor of size: (1, `height`, `width`, `channels`)
+    output: Spectrogram Tensor of size: (1, `height`, `width`, `channels`)
     """
-    #  normalize from [-32768, 32767] to [-1, 1]
-    waveform =  waveform / 32768
+    # Normalize from [-32768, 32767] to [-1, 1]
+    waveform = waveform / 32768.0
 
-    waveform = tf.convert_to_tensor(waveform, dtype=tf.float32)
+    # Get the spectrogram.
+    spectrogram = get_spectrogram(waveform)
 
-    spectogram = get_spectrogram(waveform)
-    
-    # add one dimension
-    spectogram = tf.expand_dims(spectogram, 0)
-    
-    return spectogram
+    # Add one dimension to match the expected input shape.
+    spectrogram = spectrogram[np.newaxis, ...]
+
+    return spectrogram
