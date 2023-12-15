58faab63eab3cc555a5bd96d30106159a255abac
JurgenVanpeteghem <116504027+JurgenVanpeteghem@users.noreply.github.com>
2023-12-15 11:34:54 +0100

    Update recording_helper.py

--- Coral_dev_test/recording_helper.py
+++ Coral_dev_test/recording_helper.py
@@ -1,39 +1,64 @@
-import pyaudio
-import numpy as np
+import keyboard
+import pyaudio  # recording audio
+import wave     # saving recorded audio as WAV files
 
-FRAMES_PER_BUFFER = 3200
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 16000
-p = pyaudio.PyAudio()
+# Global variable to track recording status
+recording = False
 
+# Function to record audio from the microphone
+def record_audio(filename):
 
-def record_audio():
-    stream = p.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
-        input=True,
-        frames_per_buffer=FRAMES_PER_BUFFER,
-        input_device_index=10
-    )
+    global recording
+    FORMAT = pyaudio.paInt16    # audio format
+    CHANNELS = 1                # number of channels
+    RATE = 16000                # sample rate
+    CHUNK = 1024                # chunk size
+
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True,
+                        frames_per_buffer=CHUNK)
 
     frames = []
-    seconds = 1
 
-    print("Listening...")
-
-    for i in range(0, int(RATE / FRAMES_PER_BUFFER * seconds)):
-        data = stream.read(FRAMES_PER_BUFFER)
+    while recording:
+        data = stream.read(CHUNK)
         frames.append(data)
 
-    print("Recording stopped")
-
+    # Stop recording
     stream.stop_stream()
     stream.close()
+    audio.terminate()
 
-    return np.frombuffer(b''.join(frames), dtype=np.int16)
+    # Save the recorded audio as a WAV file
+    with wave.open(filename, 'wb') as wf:
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(audio.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
 
+# Function to start and stop recording
+def toggle_recording():
+    global recording
+    if recording:
+        recording = False
+        print("Stopped recording")
+    else:
+        recording = True
+        print("Recording...")
 
-def terminate():
-    p.terminate()
+# Main function
+def main():
+    # The audio file where you want to save the recorded audio
+    audio_filename = "recorded_audio.wav"
+
+    # Register key events for manual recording start/stop control
+    keyboard.add_hotkey("r", toggle_recording)
+
+    while True:
+        if recording:
+            # Call record_audio when recording is enabledrr
+            record_audio(audio_filename)
+            
+if __name__ == "__main__":
+    main()
