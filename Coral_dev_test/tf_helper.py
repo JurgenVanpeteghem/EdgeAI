5aa3a8373583127a0e3b69599bdb081a3868a5d0
JurgenVanpeteghem <116504027+JurgenVanpeteghem@users.noreply.github.com>
2023-12-22 10:27:50 +0100

    Update tf_helper.py

--- Coral_dev_test/tf_helper.py
+++ Coral_dev_test/tf_helper.py
@@ -1,49 +1,32 @@
 import numpy as np
+import librosa
+from scipy import ndimage
 
 # Set the seed value for experiment reproducibility.
 seed = 42
 np.random.seed(seed)
 
-def stft(waveform, frame_length=255, frame_step=128):
-    window = np.hanning(frame_length)
-    num_frames = 1 + (len(waveform) - frame_length) // frame_step
-    frames = np.array([waveform[i * frame_step:i * frame_step + frame_length] * window for i in range(num_frames)])
-    stft_result = np.fft.fft(frames, axis=-1)
-    return np.abs(stft_result)
-
-
 def get_spectrogram(waveform):
-    # Zero-padding for an audio waveform with less than 16,000 samples.
-    input_len = 16000
-    waveform = waveform[:input_len]
-    zero_padding = np.zeros(16000 - len(waveform), dtype=np.float32)
-    # Concatenate the waveform with zero_padding, ensuring all audio clips are of the same length.
-    equal_length = np.concatenate([waveform, zero_padding])
+    spectrograms = []
 
-    # Convert the waveform to a spectrogram via STFT.
-    spectrogram = stft(equal_length)
+    # Generate spectrogram using librosa
+    spectrogram = librosa.feature.melspectrogram(y=waveform, sr=16000)
+    spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
 
-    # Add a `channels` dimension.
-    spectrogram = spectrogram[..., np.newaxis]
-    
-    return spectrogram
+    # Resize spectrogram to a fixed shape without TensorFlow
+    target_shape = (128, 128)
+    spectrogram = ndimage.zoom(
+        spectrogram, (target_shape[0] / spectrogram.shape[0], target_shape[1] / spectrogram.shape[1]))
 
-def preprocess_audiobuffer(waveform, expected_shape):
-    """
-    waveform: ndarray of size (16000, )
-    
-    output: Spectrogram Tensor of size: (1, `height`, `width`, `channels`)
-    """
-    # Normalize from [-32768, 32767] to [-1, 1]
-    waveform = waveform / 32768.0
+    # Ensure the shape matches the target_shape
+    if spectrogram.shape != target_shape:
+        spectrogram = spectrogram[:target_shape[0], :target_shape[1]]
 
-    # Get the spectrogram.
-    spectrogram = get_spectrogram(waveform)
+    # Add a channel dimension
+    spectrogram = np.expand_dims(spectrogram, axis=-1)
 
-    # Resize or pad the spectrogram to match the expected input shape
-    spectrogram = np.resize(spectrogram, expected_shape)
+    spectrograms.append(spectrogram)
 
-    # Add one dimension to match the expected input shape.
-    spectrogram = np.expand_dims(spectrogram, 0)
+    spectrograms = np.array(spectrograms)
 
-    return spectrogram
+    return spectrograms
