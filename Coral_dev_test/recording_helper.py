a2658af2df6e6ee6dd0ad0aa6094547b03d14fef
JurgenVanpeteghem <116504027+JurgenVanpeteghem@users.noreply.github.com>
2023-12-27 17:37:18 +0100

    Update recording_helper.py

--- Coral_dev_test/recording_helper.py
+++ Coral_dev_test/recording_helper.py
@@ -9,43 +9,41 @@ RATE = 44100
 def record_audio(frame_size):
     audio = pyaudio.PyAudio()
 
-    try:
-        stream = audio.open(
-            format=FORMAT,
-            channels=CHANNELS,
-            rate=RATE,
-            input=True,
-            input_device_index=0,  # Adjust this index based on your microphone device
-            frames_per_buffer=frame_size
-        )
+    stream = audio.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
+        input=True,
+        input_device_index=0,  # Adjust this index based on your microphone device
+        frames_per_buffer=frame_size
+    )
 
-        print("Start recording...")
+    print("Start recording...")
 
-        frames = []
-        seconds = 1  # Adjust this duration based on your requirements
+    frames = []
+    seconds = 1  # Adjust this duration based on your requirements
 
-        for _ in range(0, int(RATE / frame_size * (seconds * 1000 / FRAMES_PER_BUFFER))):
-            data = stream.read(frame_size)
-            frames.append(data)
+    for _ in range(0, int(RATE / frame_size * (seconds * 1000 / FRAMES_PER_BUFFER))):
+        data = stream.read(frame_size)
+        frames.append(data)
 
-        print("Recording stopped")
+    print("Recording stopped")
 
-        # Convert the frames to a NumPy array
-        audio_data = np.frombuffer(b''.join(frames), dtype=np.int16)
+    # Convert the frames to a NumPy array
+    audio_data = np.frombuffer(b''.join(frames), dtype=np.int16)
 
-        # Save the recorded audio as a WAV file
-        with wave.open("recorded.wav", 'wb') as wf:
-            wf.setnchannels(CHANNELS)
-            wf.setsampwidth(audio.get_sample_size(FORMAT))
-            wf.setframerate(RATE)
-            wf.writeframes(b''.join(frames))
+    stream.stop_stream()
+    stream.close()
 
-        return audio_data
+    # Save the recorded audio as a WAV file
+    with wave.open("recorded.wav", 'wb') as wf:
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(audio.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(b''.join(frames))
 
-    finally:
-        stream.stop_stream()
-        stream.close()
-        audio.terminate()
+    return audio_data
+
 
 def terminate():
     audio.terminate()
