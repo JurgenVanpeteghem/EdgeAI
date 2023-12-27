a77c4008eadd132e12d84c6d17d376b120a3bf5c
JurgenVanpeteghem <116504027+JurgenVanpeteghem@users.noreply.github.com>
2023-12-27 21:53:05 +0100

    Final version of the project

---
+++ Coral_dev_project/tf_helper.py
@@ -0,0 +1,32 @@
+import numpy as np
+import librosa
+from scipy import ndimage
+
+# Set the seed value for experiment reproducibility.
+seed = 42
+np.random.seed(seed)
+
+def get_spectogram(waveform):
+    spectrograms = []
+
+    # Generate spectrogram using librosa
+    spectrogram = librosa.feature.melspectrogram(y=waveform, sr=44100)
+    spectrogram = librosa.power_to_db(spectrogram, ref=np.max)
+
+    # Resize spectrogram to a fixed shape without TensorFlow
+    target_shape = (128, 128)
+    spectrogram = ndimage.zoom(
+        spectrogram, (target_shape[0] / spectrogram.shape[0], target_shape[1] / spectrogram.shape[1]))
+
+    # Ensure the shape matches the target_shape
+    if spectrogram.shape != target_shape:
+        spectrogram = spectrogram[:target_shape[0], :target_shape[1]]
+
+    # Add a channel dimension
+    spectrogram = np.expand_dims(spectrogram, axis=-1)
+
+    spectrograms.append(spectrogram)
+
+    spectrograms = np.array(spectrograms)
+
+    return spectrograms
