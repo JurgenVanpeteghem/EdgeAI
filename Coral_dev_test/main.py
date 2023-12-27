0c4a0e47f915e436436981c1fde6ee5511d8bf0a
JurgenVanpeteghem <116504027+JurgenVanpeteghem@users.noreply.github.com>
2023-12-27 16:41:08 +0100

    Update main.py

--- Coral_dev_test/main.py
+++ Coral_dev_test/main.py
@@ -9,7 +9,7 @@ from tf_helper import get_spectogram
 commands = ['drie', 'een', 'klaar', 'licht', 'stop', 'uit']
 
 # Audio threshold
-audio_threshold = 0.4  # Adjust this value based on your environment
+energy_threshold = 35  # Adjust this value based on your environment
 
 # Load the TensorFlow Lite model.
 interpreter = tflite.Interpreter(model_path="keyword_recognition_model_full_data.tflite")
